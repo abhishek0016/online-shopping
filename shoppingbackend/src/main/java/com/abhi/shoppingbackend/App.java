95a7dbf9aed0bf25c1914159eea18104f9cdc347
abhishek <abhishek.email0016@gmail.com>
2018-12-08 11:43:08 +0530

    My Project Setup Completed Successfully !!

---
+++ shoppingbackend/src/main/java/com/abhi/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.abhi.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
